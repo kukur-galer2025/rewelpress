cbc8a81f7463a7849930673a58aee97932663f4d
kukur dell <kukurdell@gmail.com>
2026-07-23 10:51:21 +0700

    Final cleanup: remove temporary scratch scripts

--- scratch/check_reviews.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../core/Database.php';
-$db = new Database();
-$db->query("SHOW COLUMNS FROM reviews");
-print_r($db->resultSet());
